f114a66aa12c8cc45111448de48cba97e86eaefe
Alpha Olomi <alphaolomi@gmail.com>
2020-09-18 17:10:40 +0300

    wip: add db support

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 mod handlers;
+mod errors;
+mod models;
+mod schema;
+
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
 
 /** dependencies here */
 use actix_web::{web, App, HttpServer};
+use diesel::r2d2::ConnectionManager;
+use diesel::PgConnection;
 
 
 // module declaration here
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
     std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    println!("Hello, world!");
+
+    // create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
 
     // Start http server
     HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
